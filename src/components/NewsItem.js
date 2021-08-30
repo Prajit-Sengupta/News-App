34b9ddcb4d2d7c5b11759a46669bb35352cee272
Prajit-Sengupta <ratulsengupta8@gmail.com>
2021-08-31 00:03:02 +0530

    Added button functionality

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} />
+                    <img src={imageUrl? imageUrl: "https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA="} alt="" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
                             Read more..
                         </a>
                     </div>
